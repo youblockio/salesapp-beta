87bcf4d218f38a89109e66498c6fa3d56a445cd1
raimanish9894 <raimanish9894@gmail.com>
2023-02-17 15:13:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Page from "./components/Page";
+import Page3 from "./components/Page3";
+import Page4 from "./components/Page4";
 
 function App() {
   return (
-    
+    <>
       <Page/>
+      <Page3 />
+      <Page4 />
+    </>
+      
     
   );
 }

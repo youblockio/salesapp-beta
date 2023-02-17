610493aed284d92f9db357aed4ee31ced7df59da
Pankaj Kumar <raipankaj150@gmail.com>
2023-02-17 15:01:34 +0530

    page 1-2

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -49,21 +49,56 @@ const Page = () => {
 
         <img src={image11} className={styles.image_11}></img>
       </div>
-      <div style={{ backgroundColor: 'black', marginTop: -10,color: 'white' }}>
+      <div style={{ backgroundColor: 'black', marginTop: -10, color: 'white',paddingBottom:40 }}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div
-            className={styles.line_02}
-          ></div>
+          <div className={styles.line_02}></div>
           <div>
             <h2>VOTRE PROJET PHOTOVOLTAIQUE</h2>
           </div>
-          <div
-            className={styles.line_02}
-          ></div>
+          <div className={styles.line_02}></div>
         </div>
         <h2>LE DIMENSIONNEMENT DE VOS FUTURES INSTALLATIONS SOLAIRES</h2>
-        <br/>
-        <h2 style={{fontWeight:200}}>TYPE D'INSTALLATION PV</h2>
+        <br />
+        <h2 style={{ fontWeight: 200 }}>TYPE D'INSTALLATION PV</h2>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div
+            style={{
+              backgroundColor: 'rgb(79, 175, 11)',
+              width: '40%',
+              marginLeft: '10%',
+              paddingLeft: 20,
+              paddingBottom:40
+            }}
+          >
+            <h3>Nombre de modules :</h3>
+            <span style={{ display: 'flex', width: '100%' }}>
+              <input className={styles.input02} /> &nbsp;
+              <h1 style={{ margin: 0 }}>panneaux</h1>
+            </span>
+
+            <h3>Surface PV :</h3>
+            <span style={{ display: 'flex', width: '100%' }}>
+              <input className={styles.input02} /> &nbsp;
+              <h1 style={{ margin: 0 }}>m2 environ</h1>
+            </span>
+
+            <h3>Puissance installée:</h3>
+            <span style={{ display: 'flex', width: '100%' }}>
+              <input className={styles.input02} /> &nbsp;
+              <h1 style={{ margin: 0 }}>KWC</h1>
+            </span>
+
+            <h3>Production annuelle PV:</h3>
+            <span style={{ display: 'flex', width: '100%' }}>
+              <input className={styles.input02} /> &nbsp;
+              <h1 style={{ margin: 0 }}>W + OU - 10%</h1>
+            </span>
+          </div>
+          <div style={{ width: '40%',display:'flex',alignItems:'center' }}>
+          <label for="file-upload">Uplaod Image:</label>
+          <input type="file" id="file-upload" name="file-upload"/>
+          </div>
+        </div>
       </div>
     </>
   );

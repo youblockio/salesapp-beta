33966a62b23ce9f4e97557a8fe09274e3b4c17de
raimanish9894 <raimanish9894@gmail.com>
2023-03-03 16:33:59 +0530

    update

--- src/components/InvoicePage.js
+++ src/components/InvoicePage.js
@@ -135,7 +135,7 @@ const InvoicePage = () => {
       const imgData = canvas.toDataURL("image/png", 0.5); // Use a quality of 0.5 (50%)
       let yOffset = 0;
       const pageData = canvas.toDataURL("image/jpeg", 1.0);
-      const totalPages = Math.ceil(9000 / 1500);
+      const totalPages = Math.ceil(10000 / 1500);
       pdf.setFont("helvetica", "", "StandardEncoding"); // Use Standard Encoding for text compression
       for (let i = 1; i < totalPages; i++) {
         pdf.addImage(pageData, "JPEG", 0, yOffset, imgWidth, imgHeight);
